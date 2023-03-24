2ce3ce628c3de006b53f2010960ec94acdda4dbe
Elias notebook <eliasbragga44@gmail.com>
2023-03-24 14:08:20 -0300

    adding sass and creating components folder

---
+++ src/components/card/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './index.scss'
+
+export function Card() {
+    
+    return (
+        <div>
+            <h1 className="card">Card</h1>
+        </div>
+    )
+}
