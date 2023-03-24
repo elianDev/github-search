ebf6c7a448211a451802369bf9067ec651ae0a3e
Elias notebook <eliasbragga44@gmail.com>
2023-03-24 13:04:38 -0300

    adding sass on project

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import './styles/variables/index.scss'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <div>
+        <h1 className='teste'>Teste Sass</h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
