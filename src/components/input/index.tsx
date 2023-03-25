7d96b81dfcfa8fa1e166e8caa7b51debd1b4a877
Elias notebook <eliasbragga44@gmail.com>
2023-03-25 16:05:47 -0300

    feat:creating-service-and-pushing-user-data

--- src/components/input/index.tsx
+++ src/components/input/index.tsx
@@ -1,28 +1,48 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { MyContext } from "../../context/MyContext";
 
-export function Input() {
-  const { handleSubmit } = useContext(MyContext);
-  // import a função handleSubmit do contexto
-  const [value, setValue] = useState("");
+export function Input(): JSX.Element {
 
-  const handleClick = (e: React.FormEvent<HTMLFormElement>) => {
+  interface User {
+    name: string
+    followers: number
+    following: number
+    visit_url: string
+    repos: number
+    description?: string
+  }
+
+  const { response, handleSubmit } = useContext(MyContext);
+  // import a função handleSubmit do contexto
+  const [value, setValue] = useState<string>("");
+  const [data, setData] = useState<User>()
+
+  useEffect(() => {
+    setData(response)
+  }, [response])
+
+  const handleClick = async (e: React.FormEvent<HTMLFormElement>): Promise<any> => {
     e.preventDefault()
     handleSubmit(value)
+    setValue("")
   }
 
   return (
-    <form onSubmit={handleClick}>
-      {/* Ao submit, executa a função handleSubmit enviando o argumento value */}
-      <h1>Input</h1>
-      <label htmlFor="name">Digite o nome de usuário: </label>
-      <input
-        id="name"
-        type="text"
-        value={value}
-        onChange={() => setValue(value)}
-      />
-      <button>Botão</button>
-    </form>
+    <div>
+
+      <form onSubmit={handleClick}>
+        {/* Ao submit, executa a função handleSubmit enviando o argumento value */}
+        <label htmlFor="name">Digite o nome de usuário: </label>
+        <input
+          id="name"
+          type="text"
+          value={value}
+          onChange={(item) => setValue(item.target.value.toLowerCase().split(' ').join(''))}
+        />
+        <button>Botão</button>
+
+      </form>
+      <div>{data && <p>Nome do usuário: {data.name}</p>}</div>
+    </div>
   );
 }
